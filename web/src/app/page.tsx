625f90e9113718f6a95f68b01e698d03cdd41cb4
Lucas Alves <lucasdolohov@gmail.com>
2023-06-14 15:16:47 -0300

    feat(front): layout home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+
+import nlwLogo from '@/assets/nlw-logo.svg'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -8,6 +13,56 @@ export default function Home() {
 
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+
+        {/* Sing in */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="logo nlw" />
+
+          <div className="max-w-[420px] space-y-4">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
+          >
+            cadastrar lembrança
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://www.rocketseat.com.br/"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       {/* Right */}
@@ -15,7 +70,10 @@ export default function Home() {
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não reistrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">
+            <a
+              href=""
+              className="underline transition-colors hover:text-gray-50"
+            >
               criar agora!
             </a>
           </p>
